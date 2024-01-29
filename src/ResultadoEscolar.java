704d8ff47d183cf27c81bcc4cb1a5eed622c67f6
Dierlisson Justiniano <dierlisson1@gmail.com>
2024-01-29 19:01:55 -0300

    CONDICAO TERNARIA

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,22 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
-        if (nota>=7) {
-            System.out.println("Aprovado");
-        }
-        else if (5 <= nota && nota < 7) {
-            System.out.println("Prova Recuperação");
-        }
-        else{
-            System.out.println("Reprovado");
-        }
+        // int nota = 7;
+        // if (nota>=7) {
+        //     System.out.println("Aprovado");
+        // }
+        // else if (5 <= nota && nota < 7) {
+        //     System.out.println("Prova Recuperação");
+        // }
+        // else{
+        //     System.out.println("Reprovado");
+        // }
+        //condição ternária
+        // int nota = 5;
+        // String resultado = nota>=7 ? "Aprovado" : "Reprovado";
+        // System.out.println("resultado: " + resultado);
+        //condição ternária elseif
+        int nota=7;
+        String resultado = nota>=7 ? "Aprovado" : nota>=5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
     }
 }
